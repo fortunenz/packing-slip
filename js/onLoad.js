2ba958b91504497d07fe0de2287e05fc9610e6dc
Brian Chen <bchen.chen93@gmail.com>
2015-09-04 09:36:56 +1200

    fix: Scrolling wheel on input doens't increment

--- js/onLoad.js
+++ js/onLoad.js
@@ -2,3 +2,14 @@
 $(window).ready(function() {
   $("#loading").hide();
 });
+
+// disable mousewheel on a input number field when in focus
+// (to prevent Cromium browsers change the value when scrolling)
+$('form').on('focus', 'input[type=number]', function (e) {
+  $(this).on('mousewheel.disableScroll', function (e) {
+    e.preventDefault()
+  })
+})
+$('form').on('blur', 'input[type=number]', function (e) {
+  $(this).off('mousewheel.disableScroll')
+})
