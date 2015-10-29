53d4d50d7ec946df80ad7ca3f20547ea00621972
Brian Chen <bchen.chen93@gmail.com>
2015-10-29 19:25:35 +1300

    fix: User is no longer able to scroll input to get decimal points

--- js/onLoad.js
+++ js/onLoad.js
@@ -19,3 +19,14 @@ var sortByKey = function(array, key) {
       return ((x < y) ? -1 : ((x > y) ? 1 : 0));
   });
 };
+
+// disable mousewheel on a input number field when in focus
+// (to prevent Cromium browsers change the value when scrolling)
+$("form").on("focus", "input[type=number]", function (e) {
+  $(this).on("mousewheel.disableScroll", function (e) {
+    e.preventDefault();
+  });
+});
+$("form").on("blur", "input[type=number]", function (e) {
+  $(this).off("mousewheel.disableScroll");
+});
